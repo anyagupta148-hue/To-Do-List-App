7604964236cf6aa2479a06c24321e8916c72842a
anyagupta148-hue <anya.gupta148@gmail.com>
2025-12-07 22:48:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function addTask(){
+  let task = document.getElementById("task").value;
+
+  if(task === "") return;
+
+  let li = document.createElement("li");
+  li.innerText = task;
+  document.getElementById("list").appendChild(li);
+
+  document.getElementById("task").value = "";
+}
